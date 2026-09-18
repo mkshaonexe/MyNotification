60e584defea3653aa89290189246e5d1225b63af
MK Shaon <mkshaonnew31@gmail.com>
2026-09-18 18:13:33 +0600

    chore: add WorkManager dependency and update gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,9 @@ dependencies {
     // Navigation
     implementation(libs.androidx.navigation.compose)
 
+    // WorkManager
+    implementation(libs.androidx.work.runtime.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
